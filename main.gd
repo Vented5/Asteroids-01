e0197f84ce889a0357abc5fa8688001f6226b131
Vented5 <venteddiagram@gmail.com>
2025-02-12 15:48:01 -0600

    scene(Asteroids):Sprites separted and scene impoved, spawning location path added.

--- main.gd
+++ main.gd
@@ -1,9 +1,10 @@
 extends Node
 
+@export var asteroid_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	$AsteroidTimer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,3 +13,26 @@ func _process(delta: float) -> void:
 	
 	
 	
+
+
+func _on_asteroid_timer_timeout():
+	var asteroid = asteroid_scene.instantiate() # Instantiate the scene
+	
+	# Picks a random location along the asteroid path
+	var asteroid_spawn_location = $AsteroidPath/AsteroidPathLocation
+	asteroid_spawn_location.progress_ratio = randf()
+	
+	# Sets the direciton perpendicular to the screen border
+	var direction = asteroid_spawn_location.rotation + PI / 2
+	
+	# Sets asteroid position at a random location
+	asteroid.position = asteroid_spawn_location.position 
+	
+	# Add som randomness to the direction
+	direction += randf_range(-PI / 4, PI / 4)
+	asteroid.rotation = direction 
+	
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	asteroid.linear_velocity = velocity.rotated(direction)
+	
+	add_child(asteroid)
